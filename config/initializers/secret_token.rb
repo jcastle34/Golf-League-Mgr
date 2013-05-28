c06facce5b88e9b336e50e8f12548389f6ef88c3
unknown <bcastle@BRIANCASTLE-LT.teamnorthwoods.com>
2013-05-28 15:58:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolfLeagueMgr::Application.config.secret_token = '86d9cf8aaef2bcc7d44177bcdf3bd191df4e4232f259fde364d5db438dac4c1fa903a3d236a761519bb75c6a8b16409936de1509ec9345dcf0c703d54d3fad2e'
